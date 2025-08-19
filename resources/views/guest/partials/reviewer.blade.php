d61183922cfadd16e5e48e42200673f060b33449
Thu Ta <tbeatz.feb@gmail.com>
2025-08-19 12:06:08 +0630

    reviewer register fix

--- resources/views/guest/partials/reviewer.blade.php
+++ resources/views/guest/partials/reviewer.blade.php
@@ -1,6 +1,6 @@
 <div class="w-full px-4 font-[Arial,sans-serif]">
     <div class="flex items-center justify-center w-full">
-        <a href="{{ route('guest.home', ['section' => 'register', 'role' => 'author']) }}"
+        <a href="{{ route('guest.home', ['section' => 'register', 'role' => 'reviewer']) }}"
             class="text-white font-semibold uppercase text-xs bg-[#027c7d] rounded-md hover:bg-[#027c7d]/90 border-2 border-[#d6dd42] transition px-3 py-2">
             Register Now
         </a>
